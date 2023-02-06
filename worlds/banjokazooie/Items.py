5ecb80d570468ae738a8701918d713c465f3a658
JadeCurtiss90 <nathanieltrutter@gmail.com>
2023-02-05 22:09:48 -0500

    First batch of Items/Locations

--- worlds/banjokazooie/Items.py
+++ worlds/banjokazooie/Items.py
@@ -4,9 +4,9 @@ from BaseClasses import Item, ItemClassification
 
 
 class ItemData(typing.NamedTuple):
-    name: str
+    code: typing.Optional[int]
     classification: ItemClassification
-    quantity: int
+    quantity: int = 1
 
 
 class BKItem(Item):
@@ -14,5 +14,32 @@ class BKItem(Item):
 
 
 basic_ability_table = {
-    "Jump": ItemData(13000)
+    "Jump": ItemData(130000, ItemClassification.progression),
+    "Feathery Flap": ItemData(130001, ItemClassification.progression),
+    "Flap Flip": ItemData(130002, ItemClassification.progression),
+    "Swim": ItemData(130003, ItemClassification.progression),
+    "Climb": ItemData(130004, ItemClassification.progression),
+    "Beak Barge": ItemData(130005, ItemClassification.progression),
+    "Claw Swipe": ItemData(130006, ItemClassification.progression),
+    "Roll": ItemData(130007, ItemClassification.progression),
+    "Rat-A-Tat Rap": ItemData(130008, ItemClassification.progression),
+}
+
+learned_ability_table = {
+    "Eggs": ItemData(130009, ItemClassification.progression),
+    "Talon Trot": ItemData(130010, ItemClassification.progression),
+    "Beak Buster": ItemData(130011, ItemClassification.progression),
+    "Flight": ItemData(99999, ItemClassification.progression),
+    "Shock Spring Jump": ItemData(99999, ItemClassification.progression),
+
+}
+
+progression_item_table = {
+    "Jiggy": ItemData(140000, ItemClassification.progression),
+    "Note": ItemData(140001, ItemClassification.progression),
+    "Mumbo Token": ItemData(140002, ItemClassification.progression),
+}
+
+useful_item_table = {
+    "Honeycomb Piece": ItemData(150000, ItemClassification.useful),
 }
