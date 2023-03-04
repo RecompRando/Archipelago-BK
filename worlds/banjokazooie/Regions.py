99f91527d139040a971f55e784101141f47fa04e
JadeCurtiss90 <nathanieltrutter@gmail.com>
2023-03-04 17:23:42 -0500

    Testing

--- worlds/banjokazooie/Regions.py
+++ worlds/banjokazooie/Regions.py
@@ -256,4 +256,4 @@ def initialize_locations(region: Region, locations, player: int):
         region.locations += [BKLocation(player, location_name, location_table[location_name].code, region)
                             for location_name in locations]
     except:
-        print(location_name for location_name in locations)
+        print(location_name for location_name in locations.keys())
