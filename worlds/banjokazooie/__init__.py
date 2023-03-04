4ab3c032fab3e7c016a3f6233f92382e0a774f34
JadeCurtiss90 <nathanieltrutter@gmail.com>
2023-03-04 15:53:02 -0500

    Testing

--- worlds/banjokazooie/__init__.py
+++ worlds/banjokazooie/__init__.py
@@ -151,6 +151,7 @@ class BanjoKazooieWorld(World):
             ["Honeycomb", "Egg Refill", "Red Feathers Refill", "Gold Feathers Refill", "Extra Life"])
 
     def initialize_events(self) -> None:
+        print(self.multiworld.get_filled_locations())
         for location, data in location_table.items():
             if data.code is None:
                 if "Jiggy Puzzle" in location:
