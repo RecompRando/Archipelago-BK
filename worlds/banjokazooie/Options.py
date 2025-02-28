2bc21202839449e1e877a7632e37976dcbcdc957
LittleCube <littlecubehax@gmail.com>
2025-02-28 02:39:56 -0500

    remove unimplemented options

--- worlds/banjokazooie/Options.py
+++ worlds/banjokazooie/Options.py
@@ -1,47 +1,47 @@
 from Options import Toggle, DefaultOnToggle, Range, DeathLink
 
 
-class ShuffleMoves(DefaultOnToggle):
-    """Shuffles moves into the item pool"""
-    display_name = "Shuffle Moves"
+# ~ class ShuffleMoves(DefaultOnToggle):
+    # ~ """Shuffles moves into the item pool"""
+    # ~ display_name = "Shuffle Moves"
 
 
-class ShuffleBasicMoves(Toggle):
-    """Shuffles basic moves into the item pool"""
-    display_name = "Shuffle Basic Moves"
+# ~ class ShuffleBasicMoves(Toggle):
+    # ~ """Shuffles basic moves into the item pool"""
+    # ~ display_name = "Shuffle Basic Moves"
 
 
-class ShuffleCheato(Toggle):
-    """Shuffles the three Cheato checks into the item pool, and includes Cheato locations in randomization"""
-    display_name = "Shuffle Cheato"
+# ~ class ShuffleCheato(Toggle):
+    # ~ """Shuffles the three Cheato checks into the item pool, and includes Cheato locations in randomization"""
+    # ~ display_name = "Shuffle Cheato"
 
 
-class ShuffleSecrets(Toggle):
-    """Shuffles the six Giant Eggs and Ice Key into the pool, and includes Cheato locations in randomization. If
-    enabled, all secret locations are available by default"""
-    display_name = "Shuffle Secrets"
+# ~ class ShuffleSecrets(Toggle):
+    # ~ """Shuffles the six Giant Eggs and Ice Key into the pool, and includes Cheato locations in randomization. If
+    # ~ enabled, all secret locations are available by default"""
+    # ~ display_name = "Shuffle Secrets"
 
 
-class LevelRandomizer(Toggle):
-    """Randomizes most level entrances. Mad Monster Mansion cannot be randomized. If ShuffleMoves is disabled,
-    the first level will always be Mumbo's Mountain"""
-    display_name = "Level Randomizer"
+# ~ class LevelRandomizer(Toggle):
+    # ~ """Randomizes most level entrances. Mad Monster Mansion cannot be randomized. If ShuffleMoves is disabled,
+    # ~ the first level will always be Mumbo's Mountain"""
+    # ~ display_name = "Level Randomizer"
 
 
-class CorrectPads(DefaultOnToggle):
-    """Some Shock Spring Pads and Flight Pads are always usable without learning the corresponding moves. If this is
-    turned on, those pads will be replaced with regular pads that require those moves"""
-    display_name = "Correct Pads"
+# ~ class CorrectPads(DefaultOnToggle):
+    # ~ """Some Shock Spring Pads and Flight Pads are always usable without learning the corresponding moves. If this is
+    # ~ turned on, those pads will be replaced with regular pads that require those moves"""
+    # ~ display_name = "Correct Pads"
 
 
-class SkipFurnaceFun(Toggle):
-    """Skips Furnace Fun entirely. When you get to it, the game will immediately behave as though you completed it"""
-    display_name = "Skip Furnace Fun"
+# ~ class SkipFurnaceFun(Toggle):
+    # ~ """Skips Furnace Fun entirely. When you get to it, the game will immediately behave as though you completed it"""
+    # ~ display_name = "Skip Furnace Fun"
 
 
-class RemoveNoteDoors(Toggle):
-    """Removes all Note Doors from the game"""
-    display_name = "Remove Note Doors"
+# ~ class RemoveNoteDoors(Toggle):
+    # ~ """Removes all Note Doors from the game"""
+    # ~ display_name = "Remove Note Doors"
 
 
 class NumberOfJiggies(Range):
@@ -52,24 +52,24 @@ class NumberOfJiggies(Range):
     default = 100
 
 
-class NumberOfMumboTokens(Range):
-    """How many Mumbo Tokens exist. There will always be enough to afford every transformation. Transformation
-    requirements will dynamically change based on the number of Mumbo Tokens that exist"""
-    range_start = 0
-    range_end = 112
-    default = 112
+# ~ class NumberOfMumboTokens(Range):
+    # ~ """How many Mumbo Tokens exist. There will always be enough to afford every transformation. Transformation
+    # ~ requirements will dynamically change based on the number of Mumbo Tokens that exist"""
+    # ~ range_start = 0
+    # ~ range_end = 112
+    # ~ default = 112
 
 
 bk_options = {
-    "shuffle_moves": ShuffleMoves,
-    "shuffle_basic_moves": ShuffleBasicMoves,
-    "shuffle_cheato": ShuffleCheato,
-    "shuffle_secrets": ShuffleSecrets,
-    "level_randomizer": LevelRandomizer,
-    "correct_pads": CorrectPads,
-    "skip_furnace_fun": SkipFurnaceFun,
-    "remove_note_doors": RemoveNoteDoors,
+    # ~ "shuffle_moves": ShuffleMoves,
+    # ~ "shuffle_basic_moves": ShuffleBasicMoves,
+    # ~ "shuffle_cheato": ShuffleCheato,
+    # ~ "shuffle_secrets": ShuffleSecrets,
+    # ~ "level_randomizer": LevelRandomizer,
+    # ~ "correct_pads": CorrectPads,
+    # ~ "skip_furnace_fun": SkipFurnaceFun,
+    # ~ "remove_note_doors": RemoveNoteDoors,
     "number_of_jiggies": NumberOfJiggies,
-    "number_of_mumbo_tokens": NumberOfMumboTokens,
-    "death_link": DeathLink
+    # ~ "number_of_mumbo_tokens": NumberOfMumboTokens,
+    # ~ "death_link": DeathLink
 }
